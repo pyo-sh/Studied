28816b281fb3c08ccd8d19072586a079faaf7fa5
pyo-sh <pygosky@gmail.com>
2019-11-25 20:58:31 +0900

    Algorithm : 백준 알고리즘 10953 문제

---
+++ Algorithm/백준알고리즘/N_10953.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main{
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n, A, B;
+        n = Integer.parseInt(br.readLine());
+        
+        String str;
+        String[] split;
+        for(int i = 0; i < n; ++i){
+            str = br.readLine();
+            split = str.split(",");
+            A = Integer.parseInt(split[0]);
+            B = Integer.parseInt(split[1]);
+            bw.write(A+B + "\n");
+        }
+        
+        br.close();
+        bw.close();
+    }
+}
