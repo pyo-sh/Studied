ae181c0ed9ca688fcb2e46cc4312dec61c0a3a40
pyo-sh <pygosky@gmail.com>
2020-03-08 23:03:24 +0900

    Python : Library 연습

---
+++ Python/Test.py
@@ -0,0 +1,20 @@
+# class Person:    # 클래스
+#     def __init__(self, name, age, address):
+#         self.name = name
+#         self.age = age
+#         self.address = address
+ 
+#     def greeting(self):
+#         print('안녕하세요. 저는 {0}입니다.'.format(self.name))
+
+
+# import person    # import로 person 모듈을 가져옴
+ 
+# # 모듈.클래스()로 person 모듈의 클래스 사용
+# maria = person.Person('마리아', 20, '서울시 서초구 반포동')
+# maria.greeting()
+
+# import square2               # import로 square2 모듈을 가져옴
+ 
+# print(square2.base)          # 모듈.변수 형식으로 모듈의 변수 사용
+# print(square2.square(10))    # 모듈.함수() 형식으로 모듈의 함수 사용
